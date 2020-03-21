7d3c2dee31a25826cfa38f318d7a1128b114dc6b
Dupton <dupton@126.com>
2020-03-21 22:33:12 +0800

    clean all

--- Codes/bat/ForDel.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-title Hi!
-color 1a
-echo.
-for /l %%v in (4, -1, 1) do del %%v.txt
-echo.
-pause>nul
--- Codes/bat/UPan.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-md F:\U-pan\tools\UnDriver.{645ff040-5081-101b-9f08-00aa002f954e} > nul
-if exist Y:\nul goto delete
-subst Y: F:\U-pan\tools\UnDriver.{645ff040-5081-101b-9f08-00aa002f954e}
-goto end
-:delete
-subst /d Y:
-:end
